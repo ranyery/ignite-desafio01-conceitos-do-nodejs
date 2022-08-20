1b7adcc166a6683d97a4fe1b00ca4ffd745fff3f
ranyery <ranyerysc@gmail.com>
2022-08-20 19:11:17 -0300

    feat: adiciona logica no metodo para obter lista de todos e no middlaware que verifica existencia de usuario

--- src/index.js
+++ src/index.js
@@ -11,7 +11,15 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  if (!username) return response.status(400).send();
+
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).send();
+
+  request["user"] = user;
+
+  next();
 }
 
 app.post("/users", (request, response) => {
@@ -24,7 +32,10 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const user = request.user;
+  const { todos } = user;
+
+  return response.send(todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
